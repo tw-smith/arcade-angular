d9ebd88c8f20354b2d9c9ed7fce011f8d5dd3a21
tw-smith <92607103+tw-smith@users.noreply.github.com>
2023-10-28 16:31:32 +0100

    fix score count bug

--- src/app/game-screen/game-screen.component.ts
+++ src/app/game-screen/game-screen.component.ts
@@ -16,6 +16,7 @@ export class GameScreenComponent implements OnInit{
   private ctx!: CanvasRenderingContext2D
   food: Food = new Food()
   game: GameParameters = new GameParameters(3, 1)
+  player_role: number = 0
 
 
 
@@ -49,6 +50,8 @@ export class GameScreenComponent implements OnInit{
   }
 
   ngOnInit() {
+    this.game.gameOverFlag = false;
+    console.log(this.game.gameOverFlag)
     this.canvas.nativeElement.width = 600;
     this.canvas.nativeElement.height = 600;
     this.ctx = this.canvas.nativeElement.getContext('2d') as CanvasRenderingContext2D
@@ -64,6 +67,7 @@ export class GameScreenComponent implements OnInit{
     // Set snakes
     this.socket.on('set_p1', () => {
       console.log('set_p1')
+      this.player_role = 1
       this.home_snake = new Snake('host', this.game)
       this.away_snake = new Snake('client', this.game)
       this.socket.emit('set_initial_food', {x: this.food.x, y: this.food.y})
@@ -76,7 +80,8 @@ export class GameScreenComponent implements OnInit{
     })
 
     this.socket.on('set_p2', () => {
-      console.log('set_p1')
+      console.log('set_p2')
+      this.player_role = 2
       this.home_snake = new Snake('client', this.game)
       this.away_snake = new Snake('host', this.game)
       this.socket.on('set_initial_food', (data: {x: number, y: number}) => {
@@ -187,7 +192,12 @@ export class GameScreenComponent implements OnInit{
         x: this.home_snake.segments[this.home_snake.segments.length-1].x,
         y: this.home_snake.segments[this.home_snake.segments.length-1].y,
       })
-      this.game.increase_p1_score(1)
+      if (this.player_role == 1) {
+        this.game.increase_p1_score(1)
+      }
+      if (this.player_role == 2) {
+        this.game.increase_p2_score(1)
+      }
       this.sendFoodUpdate(this.food.x, this.food.y)
       this.sendGameParamsUpdate()
     }
